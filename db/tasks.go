8825e253379ff2f3d6f21de78b21090d16325f39
M Ilham Hidayat <ilham@cohive.space>
2018-11-27 08:01:38 +0700

    Reading from buckets

--- db/tasks.go
+++ db/tasks.go
@@ -55,3 +55,25 @@ func itob(id int) []byte {
 func btoi(b []byte) int {
 	return int(binary.BigEndian.Uint64(b))
 }
+
+func GetAllTask() ([]Task, error) {
+	var tasks []Task
+
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
